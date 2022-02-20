e01338a03c847734f2538a0764fee0960fc98848
glesys-andreas <58209007+glesys-andreas@users.noreply.github.com>
2022-02-20 13:11:59 +0100

    Use curl instead of wget, to keep it consistent through script
    Co-authored-by: Martin Bagge / brother <brother@persilja.net>

--- rancher/install-rancher.sh
+++ rancher/install-rancher.sh
@@ -247,7 +247,7 @@ curl https://releases.rancher.com/install-docker/20.10.sh | sh
 ### Download dependency
 
 # rke
-wget https://github.com/rancher/rke/releases/download/v1.3.7/rke_linux-amd64
+curl -LO https://github.com/rancher/rke/releases/download/v1.3.7/rke_linux-amd64
 mv rke_linux-amd64 /usr/local/bin/rke
 chmod +x  /usr/local/bin/rke
 
